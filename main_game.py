4911367fb9131f991d3eeaa860958387d24a6b09
RohanJha23 <rohankaly2301@gmail.com>
2023-08-14 17:54:52 +0530

    Added a Start Button

--- main_game.py
+++ main_game.py
@@ -2,7 +2,6 @@ import pygame
 
 pygame.init()
 
-running = True
 # Font that is used to render the text
 font20 = pygame.font.Font('freesansbold.ttf', 20)
 
@@ -11,61 +10,162 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 
+game_state = "start_menu"
 # Basic parameters of the screen
 WIDTH, HEIGHT = 900, 600
-
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong")
-# Used to adjust the frame rate
+
 clock = pygame.time.Clock()
 FPS = 30
 
+# Striker class
+
+def draw_start_menu():
+    screen.fill((0, 0, 0))
+    font_1 = pygame.font.Font(None, 50)
+    title = font_1.render('Pong\nBy Rohan', True, (255, 255, 255))
+    start_button = font_1.render('Start', True, (255, 255, 255))
+    screen.blit(title, (WIDTH/2 - title.get_width()/2, HEIGHT/2 - title.get_height()/2))
+    screen.blit(start_button, (WIDTH/2 - start_button.get_width()/2, HEIGHT/2 + start_button.get_height()/2))
+    pygame.display.update()
+
 class Striker:
+    # Take the initial position, dimensions, speed and color of the object
+    def __init__(self, posx, posy, width, height, speed, color):
+        self.posx = posx
+        self.posy = posy
+        self.width = width
+        self.height = height
+        self.speed = speed
+        self.color = color
+        # Rect that is used to control the position and collision of the object
+        self.geekRect = pygame.Rect(posx, posy, width, height)
+        # Object that is blit on the screen
+        self.geek = pygame.draw.rect(screen, self.color, self.geekRect)
 
-    def _init_(self,posx,posy,width,height,speed,colour):
-        self.posx=posx
-        self.posy=posy
-        self.width=width
-        self.height=height
-        self.speed=speed
-        self.colour=colour
+    # Used to display the object on the screen
+    def display(self):
+        self.geek = pygame.draw.rect(screen, self.color, self.geekRect)
 
-        #Create Rectangle to display on screen
-        self.Rectangle= pygame.Rect(posx,posy,width,height)
-        #blit on screen or draw on screen//creation only
-        self.RectDraw= pygame.draw.rect(screen,self.colour,self.Rectangle)
+    def update(self, yFac):
+        self.posy = self.posy + self.speed*yFac
 
-        def display(self):
-            #function to actually draw on screen
-            self.RectDraw= pygame.draw.rect(screen,self.colour,self.Rectangle)
+        # Restricting the striker to be below the top surface of the screen
+        if self.posy <= 0:
+            self.posy = 0
+        # Restricting the striker to be above the bottom surface of the screen
+        elif self.posy + self.height >= HEIGHT:
+            self.posy = HEIGHT - self.height
 
-        def update(self,yFac):
-            #for updating after key movement!
-            self.posy=self.posy +self.speed*yFac
+        # Updating the rect with the new values
+        self.geekRect = pygame.Rect(self.posx, self.posy, self.width, self.height)
+
+    def displayScore(self, text, score, x, y, color):
+        text = font20.render(text + str(score), True, color)
+        textRect = text.get_rect()
+        textRect.center = (x, y)
+
+        screen.blit(text, textRect)
+
+    def getRect(self):
+        return self.geekRect
+
+# Ball class
+
+class Ball:
+    def __init__(self, posx, posy, radius, speed, color):
+        self.posx = posx
+        self.posy = posy
+        self.radius = radius
+        self.speed = speed
+        self.color = color
+        self.xFac = 1
+        self.yFac = -1
+        self.ball = pygame.draw.circle(
+            screen, self.color, (self.posx, self.posy), self.radius)
+        self.firstTime = 1
+
+    def display(self):
+        self.ball = pygame.draw.circle(
+            screen, self.color, (self.posx, self.posy), self.radius)
+
+    def update(self):
+        self.posx += self.speed * self.xFac
+        self.posy += self.speed * self.yFac
+
+        # If the ball hits the top or bottom surfaces,
+        # then the sign of yFac is changed and
+        # it results in a reflection
+        if self.posy <= 0 or self.posy >= HEIGHT:
+            self.yFac *= -1
+
+        if self.posx <= 0 and self.firstTime:
+            self.firstTime = 0
+            return 1
+        elif self.posx >= WIDTH and self.firstTime:
+            self.firstTime = 0
+            return -1
+        else:
+            return 0
+
+    def reset(self):
+        self.posx = WIDTH // 2
+        self.posy = HEIGHT // 2
+        self.xFac *= -1
+        self.firstTime = 1
+
+    # Used to reflect the ball along the X-axis
+    def hit(self):
+        self.xFac *= -1
+
+    def getRect(self):
+        return self.ball
+
+# Game Manager
+
+def main():
+    global game_state
+
+    running = True
+
+    while running:
+        if game_state == "start_menu":
+            draw_start_menu()
+
+        elif game_state == "game":
+            screen.fill(BLACK)  # Clear the screen only when in the game state
+
+            # Event handling
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_UP]:
+                geek2YFac = -1
+            if keys[pygame.K_DOWN]:
+                geek2YFac = 1
+            if keys[pygame.K_w]:
+                geek1YFac = -1
+            if keys[pygame.K_s]:
+                geek1YFac = 1
+            if not keys[pygame.K_UP] and not keys[pygame.K_DOWN]:
+                geek2YFac = 0
+            if not keys[pygame.K_w] and not keys[pygame.K_s]:
+                geek1YFac = 0
+
+            # Rest of your game logic
+
+            pygame.display.update()
+            clock.tick(FPS)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
 
 
-            #restricting to window limits
-            if self.posy<=0:
-                self.posy=0
-
-            #lower level restriction
-            elif self.posy + self.height >=HEIGHT:
-                self.posy =HEIGHT-self.height
-            
-            #draw new rectangle by updating
-            self.Rectangle = (self.posx,self.posy,self.width,self.height)
-
-
-        def ScoreDisp(self,text,score,x,y,colour);
-            text = font20.render(text+str(score), True, color)
-            textRect = text.get_rect()
-            textRect.center = (x, y)
- 
-            screen.blit(text, textRect)
-
-        def getRect(self):
-            return self.Rectangle
-        
 
 
 
